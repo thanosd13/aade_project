9085a90e56a6e3246516faef67785e06548529d2
thanosd13 <adimopoulos@ceid.upatras.gr>
2024-07-17 20:32:07 +0300

    new

--- models/customerModel.js
+++ models/customerModel.js
@@ -1,34 +1,38 @@
 const sequelize = require("sequelize");
 const db = require("../config/database");
 var customer = db.define(
-    "customer",
-    {
-        id: { type: sequelize.INTEGER, primaryKey: true },
-        afm: {
-            type: sequelize.STRING,
-            allowNull: false,  // Ensures AFM cannot be null
-            unique: true,      // Ensures each AFM is unique
-            validate: {
-                notEmpty: { msg: "AFM must not be empty" }  // Ensures AFM is not an empty string
-            }
-        },
-        name: { type: sequelize.STRING },
-        country: { type: sequelize.STRING },
-        city: { type: sequelize.STRING },
-        address: { type: sequelize.STRING },
-        street_number: { type: sequelize.STRING },
-        postal_code: {type: sequelize.STRING},
-        doy: { type: sequelize.STRING },
-        work: { type: sequelize.STRING(600) },
-        email: { type: sequelize.STRING },
-        tel_number:  { type: sequelize.STRING },
-        userId: { type: sequelize.INTEGER, allowNull: false, references: { model: 'user', key: 'id' } }
+  "customer",
+  {
+    id: { type: sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    afm: {
+      type: sequelize.STRING,
+      allowNull: false, // Ensures AFM cannot be null
+      unique: true, // Ensures each AFM is unique
+      validate: {
+        notEmpty: { msg: "AFM must not be empty" }, // Ensures AFM is not an empty string
+      },
     },
-    {
-        // freeze name table not using *s on name
-        freezeTableName: true,
-        // dont use createdAt/update
-        timestamps: false,
-    }
+    name: { type: sequelize.STRING },
+    country: { type: sequelize.STRING },
+    city: { type: sequelize.STRING },
+    address: { type: sequelize.STRING },
+    street_number: { type: sequelize.STRING },
+    postal_code: { type: sequelize.STRING },
+    doy: { type: sequelize.STRING },
+    work: { type: sequelize.STRING(600) },
+    email: { type: sequelize.STRING },
+    tel_number: { type: sequelize.STRING },
+    userId: {
+      type: sequelize.INTEGER,
+      allowNull: false,
+      references: { model: "user", key: "id" },
+    },
+  },
+  {
+    // freeze name table not using *s on name
+    freezeTableName: true,
+    // dont use createdAt/update
+    timestamps: false,
+  }
 );
 module.exports = customer;
